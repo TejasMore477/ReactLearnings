0345ba2230ada51da850e202b931c1f8138a4166
Tejas More <tejasmore660@gmail.com>
2024-07-24 19:31:03 +0530

    created file using react and also suing vite/cleared

---
+++ letcure2/01createreact/src/App.js
@@ -0,0 +1,8 @@
+
+function App() {
+  return (
+    <h1>hellow tejas welcome to react!</h1>
+  );
+}
+
+export default App;
---
+++ letcure2/01vitereact/src/App.jsx
@@ -0,0 +1,11 @@
+
+function App() {
+
+  return (
+    <>
+      <h1>welcome to react vite tejas!</h1>
+    </>
+  )
+}
+
+export default App
